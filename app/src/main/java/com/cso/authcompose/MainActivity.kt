6d2fd3e652e29c681fafb10d07c4a306c2a8241d
Cainã Oliveira <csoliveira01@gmail.com>
2025-07-28 20:21:40 -0400

    Adding project

---
+++ app/src/main/java/com/cso/authcompose/MainActivity.kt
@@ -0,0 +1,306 @@
+package com.cso.authcompose
+
+import android.content.Context
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Email
+import androidx.compose.material.icons.rounded.Lock
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.credentials.CredentialManager
+import androidx.credentials.CustomCredential
+import androidx.credentials.GetCredentialRequest
+import com.cso.authcompose.ui.theme.AuthComposeTheme
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+import com.google.android.libraries.identity.googleid.GoogleIdTokenParsingException
+import com.google.firebase.Firebase
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.auth
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import java.security.MessageDigest
+import java.util.UUID
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AuthComposeTheme {
+                LoginScreen()
+            }
+        }
+    }
+}
+
+
+class AuthenticationManager(val context: Context) {
+    private val auth = Firebase.auth
+
+    fun createAccountWithEmail(email: String, password: String): Flow<AuthResponse> = callbackFlow {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    trySend(AuthResponse.Success)
+                } else {
+                    trySend(AuthResponse.Error(task.exception?.message ?: "Unknown error"))
+                }
+            }
+
+        awaitClose()
+    }
+
+    fun loginWithEmail(email: String, password: String): Flow<AuthResponse> = callbackFlow {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    trySend(AuthResponse.Success)
+                } else {
+                    trySend(AuthResponse.Error(task.exception?.message ?: "Unknown error"))
+                }
+            }
+
+        awaitClose()
+    }
+
+    private fun createNonce(): String {
+        val rawNonce = UUID.randomUUID().toString()
+        val bytes = rawNonce.toByteArray()
+        val md = MessageDigest.getInstance("SHA-256")
+        val digest = md.digest(bytes)
+
+        return digest.fold("") { str, it ->
+            str + "%02x".format(it)
+        }
+    }
+
+    fun signInWithGoogle(): Flow<AuthResponse> = callbackFlow {
+        val googleIdOption = GetGoogleIdOption.Builder()
+            .setFilterByAuthorizedAccounts(false)
+            .setServerClientId(context.getString(R.string.web_client_id))
+            .setAutoSelectEnabled(false)
+            .setNonce(createNonce())
+            .build()
+
+        val request = GetCredentialRequest.Builder()
+            .addCredentialOption(googleIdOption)
+            .build()
+
+
+        try {
+            val credentialManager = CredentialManager.create(context)
+            val result = credentialManager.getCredential(
+                context = context,
+                request = request
+            )
+
+            val credential = result.credential
+            if (credential is CustomCredential) {
+                if (credential.type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL) {
+                    try {
+                        val googleIdTokenCredential =
+                            GoogleIdTokenCredential.createFrom(credential.data)
+
+                        val firebaseCredential =
+                            GoogleAuthProvider.getCredential(googleIdTokenCredential.idToken, null)
+
+                        auth.signInWithCredential(firebaseCredential)
+                            .addOnCompleteListener { task ->
+                                if (task.isSuccessful) {
+                                    trySend(AuthResponse.Success)
+                                } else {
+                                    trySend(
+                                        AuthResponse.Error(
+                                            task.exception?.message ?: "Unknown error"
+                                        )
+                                    )
+                                }
+                            }
+
+
+                    } catch (e: GoogleIdTokenParsingException) {
+                        trySend(AuthResponse.Error(message = e.message ?: ""))
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            trySend(AuthResponse.Error(message = e.message ?: ""))
+        }
+
+        awaitClose()
+    }
+}
+
+interface AuthResponse {
+    data object Success : AuthResponse
+    data class Error(val message: String) : AuthResponse
+}
+
+@Composable
+fun LoginScreen() {
+
+    var email by remember {
+        mutableStateOf("")
+    }
+
+    var password by remember {
+        mutableStateOf("")
+    }
+
+    val context = LocalContext.current
+
+    val authenticationManager = remember {
+        AuthenticationManager(context)
+    }
+
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Sign-in",
+            style = MaterialTheme.typography.displayMedium,
+            fontWeight = FontWeight.Bold
+        )
+
+        Text(
+            text = "Please fill the form to continue",
+            style = MaterialTheme.typography.titleLarge,
+            fontWeight = FontWeight.SemiBold
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { newValue ->
+                email = newValue
+            },
+            placeholder = {
+                Text(text = "E-mail")
+            },
+            leadingIcon = {
+                Icon(imageVector = Icons.Rounded.Email, contentDescription = null)
+            },
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { newValue ->
+                password = newValue
+            },
+            placeholder = {
+                Text(text = "Password")
+            },
+            leadingIcon = {
+                Icon(imageVector = Icons.Rounded.Lock, contentDescription = null)
+            },
+            visualTransformation = PasswordVisualTransformation(),
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = {
+                authenticationManager.loginWithEmail(email, password).onEach { response ->
+                    if (response is AuthResponse.Success) {
+                        print("Success")
+                    } else if (response is AuthResponse.Error) {
+                        print("Error ${response.message}")
+                    }
+                }.launchIn(coroutineScope)
+            }
+        ) {
+            Text(
+                text = "Sign-in",
+                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(vertical = 4.dp)
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 12.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "or continue with")
+        }
+
+        OutlinedButton(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = {
+                authenticationManager.signInWithGoogle().onEach { response ->
+                    if (response is AuthResponse.Success) {
+                        print("Success")
+                    } else if (response is AuthResponse.Error) {
+                        print("Error ${response.message}")
+                    }
+                }.launchIn(coroutineScope)
+            }) {
+
+//            Image(
+//                painter = painterResource(id = R.drawable.google),
+//                contentDescription = null
+//            )
+            Text(
+                text = "Sign-in with Google",
+                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.titleMedium,
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    AuthComposeTheme {
+        LoginScreen()
+    }
+}
